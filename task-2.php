080ff54a49c6136439a44da051125eaf524b0688
Shakibur123rahman <101545878+Shakibur123rahman@users.noreply.github.com>
2023-02-10 01:08:36 +0600

    Add files via upload

---
+++ task-2.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title></title>
+</head>
+<body>
+	
+	<fieldset>
+		<legend><h1><B>REGISTRATION</B></h1> </legend>
+
+	
+	<label for="name"> <B> Name :</B></label>
+	<input type="text" name="name"> <br><br>
+    <hr>
+	
+	
+	<label for="Email"><B>Email :</B></label>
+    <input type="Email" name="Email"><br><br>
+    <hr>
+
+    <label for="u_name"><B>user Name :</B></label>
+    <input type="text" name="U_name"><br><br>
+    <hr>
+
+    <label for="password"><B>password :</B></label>
+    <input type="password" name="password"><br><br>
+    <hr>
+
+    <label for="cname"><B>confirm password :</B></label>
+    <input type="text" name="cname"><br><br>
+    <hr>
+
+    <fieldset>
+    
+      <legend>Gender</legend>
+
+
+      <input type="radio" name="male">
+      <label for="male"> <B>Male</B></label>
+
+      <input type="radio" name="Fmale">
+      <label for="fmale"> <B>FMale</B></label>
+
+      <input type="radio" name="other">
+      <label for="other"><B> other</B></label><br><br>
+    
+    </fieldset><br>
+
+    <fieldset>
+
+    	<legend>Date of Birth</legend>
+
+        <input type="text" name="date">/
+        <input type="text" name="month">/
+        <input type="text" name="year"> <label>(dd/mm/yyy)</label>
+
+
+
+    </fieldset><br><br>
+
+
+    <button> Submit</button>
+    <button> Reset</button>
+
+    </fieldset>
+
+
+
+
+	
+</body>
+</html>
